95b63e199521c1865c7b6fea8167fa2647a42fc2
fjmg84 <fjmgqba@gmail.com>
2023-07-28 21:52:56 -0400

    remove StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { store } from "./stores/store";
+import { store } from "./redux/stores/store";
 import { router } from "./router";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
